525c7ac0408b0f3bd2b5c741c8bf6f3233aa5549
smal <mail@bymi.ru>
2019-02-10 23:40:36 +1000

    BlogCategoryUpdate, validate request

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -6,84 +6,85 @@ use App\Models\BlogCategory;
 use foo\bar;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseBlogAdminController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $paginator = BlogCategory::paginate(5);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $paginator = BlogCategory::paginate(5);
 
-        return view('blog.admin.categories.index', compact('paginator'));
+    return view('blog.admin.categories.index', compact('paginator'));
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $item = BlogCategory::findOrFail($id);
+    $categoryList = BlogCategory::all();
+
+    return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(BlogCategoryUpdateRequest $request, $id)
+  {
+    $item = BlogCategory::find($id);
+    if(empty($item)) {
+      return back()
+        ->withErrors(['msg' => "Запись id=[{$id}] не найдена"])
+        ->withInput();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    $data = $request->all();
+    $result = $item->fill($data)->save();
+
+    if($result) {
+      return redirect()
+        ->route('blog.admin.categories.edit', $item->id)
+        ->with(['success' => 'Успешно сохранено'])
+        ->withInput();
+    } else {
+      return back()
+        ->withErrors(['msg' => "Ошибка сохранения"])
+        ->withInput();
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $item = BlogCategory::findOrFail($id);
-        $categoryList = BlogCategory::all();
-
-        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $item = BlogCategory::find($id);
-        if(empty($item)) {
-            return back()
-                ->withErrors(['msg' => "Запись id=[{$id}] не найдена"])
-                ->withInput();
-        }
-
-        $data = $request->all();
-        $result = $item->fill($data)->save();
-
-        if($result) {
-            return redirect()
-                ->route('blog.admin.categories.edit', $item->id)
-                ->with(['success' => 'Успешно сохранено'])
-                ->withInput();
-        } else {
-            return back()
-                ->withErrors(['msg' => "Ошибка сохранения"])
-                ->withInput();
-        }
-    }
+  }
 }
